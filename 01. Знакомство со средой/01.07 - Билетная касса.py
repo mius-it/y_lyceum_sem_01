9bf56fc398fec26f3d4d1b5a984adfeb84601cc9
mius <mius@mail.ru>
2025-01-14 03:20:27 +0300

    3rd

---
+++ 01. Знакомство со средой/01.07 - Билетная касса.py
@@ -0,0 +1,5 @@
+movie = input()
+cinema = input()
+time = input()
+
+print("Билет на \"", movie, "\" в \"", cinema, "\" на", time, "забронирован.")
---
+++ 01. Знакомство со средой/01.10 - Гороскоп.py
@@ -0,0 +1,8 @@
+name = input()
+surname = input()
+animal = input()
+zodiac = input()
+
+print("Индивидуальный гороскоп для пользователя", name, surname)
+print("Кем вы были в прошлой жизни:", animal)
+print("Ваш знак зодиака - ", zodiac, ", поэтому вы - тонко чувствующая натура.")
