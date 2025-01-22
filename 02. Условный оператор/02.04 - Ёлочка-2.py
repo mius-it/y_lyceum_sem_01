edbd93088d57bff822f152cac72531e25d6202d0
mius <mius@mail.ru>
2025-01-22 11:17:00 +0300

    2.16

---
+++ 02. Условный оператор/02.04 - Ёлочка-2.py
@@ -0,0 +1,9 @@
+my_word_1 = input()
+my_word_2 = input()
+my_word_3 = input()
+
+if (my_word_1 == "раз" and my_word_2 == "два" and my_word_3 == "три" or
+        my_word_1 == "1" and my_word_2 == "2" and my_word_3 == "3"):
+    print("ГОРИ")
+else:
+    print("НЕ ГОРИ")
---
+++ 02. Условный оператор/02.14 - Да или нет.py
@@ -0,0 +1,8 @@
+str01 = input()
+str02 = input()
+
+if ((str01 == 'да' or str01 == 'нет') and
+        (str02 == 'да' or str02 == 'нет')):
+    print('ВЕРНО')
+else:
+    print('НЕВЕРНО')
