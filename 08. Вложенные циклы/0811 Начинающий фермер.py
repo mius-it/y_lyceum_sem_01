bc36b269cd5a6c8758bb97232aef81eabfd6bcfc
mius_it <mius@mail.ru>
2025-02-10 07:19:39 +0300

    0811ready

--- 08. Вложенные циклы/0811 Начинающий фермер.py
+++ 08. Вложенные циклы/0811 Начинающий фермер.py
@@ -2,3 +2,12 @@ BULL = 20000
 COW = 10000
 CALF = 5000
 
+price = int(input()) * 1000
+herd = int(input())
+
+for bulls in range (1, herd):
+    for cows in range(herd):
+        for calfs in range(herd):
+            if ((bulls * BULL + cows * COW + calfs * CALF == price) and
+                    (bulls + cows + calfs == herd)):
+                print(bulls, cows, calfs)
