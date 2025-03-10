09120bd7661c79c059f8190a6bc3e412606ad520
mius_pc <mius@mail.ru>
2025-03-10 10:47:57 +0300

    1812.3

--- 18. Знакомство со словарями/1812 Репосты.py
+++ 18. Знакомство со словарями/1812 Репосты.py
@@ -1,21 +1,25 @@
 N = int(input())
 posts = []
+totalviews = [0] * N
 item = input()
-itemtup = {'Author' : item[:item.find('опубликовал') - 1],
-           'Reposter': 'none',
-           'Views' : int(item[item.rfind(' ') + 1:])}
+itemtup = {'Poster': item[:item.find('опубликовал') - 1],
+           'Original': 'none',
+           'Views': int(item[item.rfind(' ') + 1:])}
 posts.append(itemtup)
 
 for _ in range(N - 1):
     item = input()
-    itemtup = {'Author': item[item.find('отрепостил пост у') + len('отрепостил пост у') + 1:item.find(',')],
-               'Reposter': item[:item.find('отрепостил') - 1],
+    itemtup = {'Poster': item[:item.find('отрепостил') - 1],
+               'Original': item[item.find('отрепостил пост у') + len('отрепостил пост у') + 1:item.find(',')],
                'Views': int(item[item.rfind(' ') + 1:])}
     posts.append(itemtup)
 
-print('\n'.join(str(p) for p in posts))
+#print('\n'.join(str(p) for p in posts))
 
-for i in range(N):
-    views = 0
-    views += posts[i]['Views']
-    print(views)
+for i in range(N - 1, -1, -1):
+    totalviews[i] += posts[i]['Views']
+    for j in range(N - 1, i, -1):
+        if posts[i]['Poster'] == posts[j]['Original']:
+            totalviews[i] += totalviews[j]
+
+print('\n'.join(str(t) for t in totalviews))
