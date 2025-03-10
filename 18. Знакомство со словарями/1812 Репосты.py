c2ec238bed428ead8840ef1c970ad548ef6e22f8
mius_it <mius@mail.ru>
2025-03-10 09:32:22 +0300

    1812.2

--- 18. Знакомство со словарями/1812 Репосты.py
+++ 18. Знакомство со словарями/1812 Репосты.py
@@ -2,11 +2,20 @@ N = int(input())
 posts = []
 item = input()
 itemtup = {'Author' : item[:item.find('опубликовал') - 1],
+           'Reposter': 'none',
            'Views' : int(item[item.rfind(' ') + 1:])}
 posts.append(itemtup)
-print(posts)
+
 for _ in range(N - 1):
     item = input()
-    itemtup = {'Author': item[:item.find('опубликовал') - 1],
+    itemtup = {'Author': item[item.find('отрепостил пост у') + len('отрепостил пост у') + 1:item.find(',')],
+               'Reposter': item[:item.find('отрепостил') - 1],
                'Views': int(item[item.rfind(' ') + 1:])}
     posts.append(itemtup)
+
+print('\n'.join(str(p) for p in posts))
+
+for i in range(N):
+    views = 0
+    views += posts[i]['Views']
+    print(views)
